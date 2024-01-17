599ea923dbb5bea94ed55fd44dc674a9908890fa
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2024-01-17 08:18:28 +0100

    Initial commit

---
+++ src/main/java/at/technikum/springrestbackend/controller/IndexController.java
@@ -0,0 +1,13 @@
+package at.technikum.springrestbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "World";
+    }
+}
